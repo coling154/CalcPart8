fb103e99327dbfe0cb663461b2ae9fbf68f2cd09
Colin <gasiewca@clarkson.edu>
2022-04-17 16:11:48 -0400

    finished intigral

--- calc.py
+++ calc.py
@@ -1,6 +1,7 @@
 
 
 #Dependencies
+from tkinter import Y
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
@@ -10,7 +11,7 @@ import scipy.integrate as integrate
 Yw=18
 
 def f(x):
-    return (-1/15)*x**5+(3/2)*x**4-(37/3)*x**3+46*x**2-(771/10)*x+59
+    return ((-1/15)*x**5)+((3/2)*x**4)-((37/3)*x**3)+(46*x**2)-((771/10)*x)+59
 
 #find all values where Yw==f(x)
 xPoints=np.arange(0,8,0.001)
@@ -21,16 +22,21 @@ for x in xPoints:
 # (0.943,18),(6.464,18)
 point1=[0.943,18]
 point2=[6.464,18]
-
-
+yw=np.empty(len(xPoints))
+yw.fill(Yw)
 
 #plot graph
-fig, ax=plt.subplots(figsize=(12,6))
-ax.plot(xPoints,yPoints,color='blue',label='graph data')
-plt.axhline(y=Yw,color='red',label='Yw')
+fig, ax =plt.subplots(figsize=(12,6),sharex=True)
+plt.plot(xPoints,yPoints,color='blue',label='graph data')
+plt.plot(xPoints,yw,color='red',label='Yw')
+
 plt.plot(point1[0],point1[1],marker='o',markersize=5,markeredgecolor='black',markerfacecolor='green')
 plt.plot(point2[0],point2[1],marker='o',markersize=5,markeredgecolor='black',markerfacecolor='green')
 
 plt.grid()
 plt.show()
 
+
+fx=lambda x:((-1/15)*x**5)+((3/2)*x**4)-((37/3)*x**3)+(46*x**2)-((771/10)*x)+59
+I=integrate.quad(fx,point1[0],point2[0])
+print("The intigral is"+ I[0])
